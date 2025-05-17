b6d194ae4ee4bd4322a2c841efa37d78d5b974d2
a10217 <a0409.li.ce@gmail.com>
2025-05-17 14:51:17 +0800

    vr

---
+++ home/app-files/data.js
@@ -0,0 +1,510 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5275425148976325,
+        "pitch": -0.02278732012905138,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07141958506469592,
+          "pitch": 0.1601137892779363,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0974398959858576,
+          "pitch": 0.0903519655439684,
+          "rotation": 1.5707963267948966,
+          "target": "5-125"
+        },
+        {
+          "yaw": -1.2717538636096801,
+          "pitch": 0.10171565332383992,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7466208473797273,
+          "pitch": 0.061441537811660396,
+          "title": "重陽公園入口處",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "幼稚園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1405360260119366,
+        "pitch": 0.17578789813841667,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2856570432881753,
+          "pitch": 0.2551953279325243,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.010119486578298975,
+          "pitch": 0.0744847086823377,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.1142144238484892,
+          "pitch": 0.08802919475752802,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2045392740156107,
+          "pitch": -0.02347923488304282,
+          "title": "私立幼兒園",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "重陽路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6212043533543259,
+        "pitch": 0.10946573592703857,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.045235370289671,
+          "pitch": 0.22663395793786023,
+          "rotation": 11.780972450961727,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.04879429467865748,
+          "pitch": 0.08817301319297854,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        },
+        {
+          "yaw": -1.4834359597616213,
+          "pitch": 0.14490649423333934,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "便利商店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9032592709481175,
+        "pitch": 0.14323458366833464,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6052792339643673,
+          "pitch": 0.11227832471559296,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.1379800722091655,
+          "pitch": 0.06629470316239328,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7420329838932567,
+          "pitch": -0.02216647995276233,
+          "title": "7-11",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9371267792086515,
+          "pitch": 0.01240518423422543,
+          "title": "全家",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "公車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.33692442951542745,
+        "pitch": 0.04231930881110024,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07066922581003965,
+          "pitch": 0.15795058761842817,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6744064675132009,
+          "pitch": -0.34572772148316133,
+          "title": "中國電視公司",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-125",
+      "name": "重陽路125",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5707963267948877,
+        "pitch": -0.013021325788024285,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05226424190088785,
+          "pitch": 0.04833700674172903,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.603220092000285,
+          "pitch": 0.1474865731177406,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -3.0954793279078334,
+          "pitch": 0.10935586392187702,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8766276807581193,
+          "pitch": 0.09340650559676966,
+          "title": "籃球場",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "滑梯入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7460052279488316,
+          "pitch": 0.1163341261540829,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.9191408388717832,
+          "pitch": 0.031707201501561144,
+          "rotation": 7.0685834705770345,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "松河街",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4842887030003897,
+        "pitch": 0.0652285139206441,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0525410924225502,
+          "pitch": 0.10671831204610172,
+          "rotation": 5.497787143782138,
+          "target": "5-125"
+        },
+        {
+          "yaw": -0.08814927008824469,
+          "pitch": 0.03170562841942903,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.1109913394230873,
+          "pitch": 0.06726878705107708,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6585782417182777,
+          "pitch": -0.002241958666230559,
+          "title": "基隆河河堤",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "運動場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.8961584561286458,
+        "pitch": 0.2577854607543806,
+        "fov": 1.2543877506952101
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3772045647270126,
+          "pitch": 0.13457774459663518,
+          "rotation": 5.497787143782138,
+          "target": "5-125"
+        },
+        {
+          "yaw": -0.037009910357408415,
+          "pitch": 0.02643998141675752,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -1.6928841866980182,
+          "pitch": 0.11530027106666196,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9098139560159133,
+          "pitch": -0.010719740311859738,
+          "title": "涼亭",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.13199639549916853,
+          "pitch": -0.011254663050486613,
+          "title": "單槓",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3762333486953784,
+          "pitch": -0.11218090507413336,
+          "title": "籃球場",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.770515684399392,
+          "pitch": 0.048830767609446823,
+          "title": "羽球場",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
